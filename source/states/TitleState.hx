bd166f1912ad1559c893436d41568d1e2560a122
midoribnuy740 <midoribnuy740@gmail.com>
2025-09-25 19:33:08 -0300

    title screen test

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -185,7 +185,7 @@ class TitleState extends MusicBeatState
 		}
 		else
 		{
-			gfDance.animation.addByPrefix('idle', animationName, 24, false);
+			gfDance.animation.addByPrefix('idle', animationName, 24, true);
 			gfDance.animation.play('idle');
 		}
 
@@ -247,23 +247,23 @@ class TitleState extends MusicBeatState
 	}
 
 	// JSON data
-	var characterImage:String = 'gfDanceTitle';
-	var animationName:String = 'gfDance';
+	var characterImage:String = 'sit-reimu';
+	var animationName:String = 'sit-reimu';
 
 	var gfPosition:FlxPoint = FlxPoint.get(512, 40);
 	var logoPosition:FlxPoint = FlxPoint.get(-150, -100);
 	var enterPosition:FlxPoint = FlxPoint.get(100, 576);
 	
-	var useIdle:Bool = false;
+	var useIdle:Bool = true;
 	var musicBPM:Float = 102;
 	var danceLeftFrames:Array<Int> = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
 	var danceRightFrames:Array<Int> = [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 	function loadJsonData()
 	{
-		if(Paths.fileExists('images/gfDanceTitle.json', TEXT))
+		if(Paths.fileExists('images/sit-reimu.json', TEXT))
 		{
-			var titleRaw:String = Paths.getTextFromFile('images/gfDanceTitle.json');
+			var titleRaw:String = Paths.getTextFromFile('images/sit-reimu.json');
 			if(titleRaw != null && titleRaw.length > 0)
 			{
 				try
@@ -575,7 +575,7 @@ class TitleState extends MusicBeatState
 				case 6:
 					createCoolText(['Not associated', 'with'], -40);
 				case 8:
-					addMoreText('newgrounds', -40);
+					addMoreText('Either of these', -40);
 					ngSpr.visible = true;
 				case 9:
 					deleteCoolText();
